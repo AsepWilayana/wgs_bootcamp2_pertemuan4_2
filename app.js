2f29f926995fe43036338ebfa0b41684bd30147b
AsepWilayana <asepwilayana@gmail.com>
2022-07-07 12:15:10 +0700

    quest

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var validator = require('validator');
 const readline = require('readline');
+const { Redirect } = require('react-router-dom');
+const { resolve } = require('path');
 
 // console.log(validator.isEmail('foo@bar.com')); //=> true
 // console.log(validator.isMobilePhone('083808682474', 'id-ID')); //=> true
@@ -9,31 +11,38 @@ const rl = readline.createInterface({
     output:process.stdout
 });
 
+let nama, emails;
+
+var quest_email = function(){
+    rl.question('what is your email?', (email) => {
+    validemail = validator.isEmail(email)
+        if(validemail === true){
+            emails = email
+            quest_nomortelepon();
+        }else {
+            quest_email();
+
+        }
+});
+}
+
+var quest_nomortelepon = function(){
+    rl.question('what is your Nomor Telepon?', (nomor_tlp) => {
+        validntlp = validator.isMobilePhone(nomor_tlp, 'id-ID')
+        if(validntlp === true){
+            console.log(`Nama : ${nama}`);
+            console.log(`Email : ${emails}`);
+            console.log(`Nomor Telepon : ${nomor_tlp}`);
+            return rl.close();
+        }else{
+            quest_nomortelepon();
+        }
+    });
+}
 
 
 // cara mebuat pertanyaan
 rl.question('what is your name?', (name) => {
-    rl.question('what is your email?', (email) => {
-        validemail = validator.isEmail(email)
-        if(validemail === true){
-            rl.question('what is your Nomor Telepon?', (nomor_tlp) => {
-                validntlp = validator.isMobilePhone(nomor_tlp, 'id-ID')
-                if(validntlp === true){
-                    console.log(`Nama : ${name}`);
-                    console.log(`Email : ${email}`);
-                    console.log(`Nomor Telepon : ${nomor_tlp}`);
-                    return rl.close();
-                }else{
-                    console.log(`Nama : ${name}`);
-                    console.log(`Email : ${email}`);
-                    console.log(`Nomor Telepon salah format`);
-                    return rl.close();
-                }
-            });
-        }else {
-            console.log(`Nama : ${name}`);
-            console.log('email salah');
-            return rl.close();
-        }
-    });
+    nama = name
+    quest_email();
 });
